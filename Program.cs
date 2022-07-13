ffc6a0043506f5a9904c656402d196fdad797f7c
Maryam-2222 <maryamarshad2001@gmail.com>
2022-07-12 21:37:20 -0700

    finilized project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum Of Two Numbers\n\n");
+            Console.WriteLine("Enter first number");
+            int one = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("sum " +(one+second)) ;
+        }
+    }
+}
